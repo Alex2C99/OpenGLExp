3fd4c2b2ac101f7a8fe669cf9774557b5cce0b60
Alexey Scripkin <a.scripkin@gmail.com>
2016-10-04 14:21:02 +0300

    VertexArray

---
+++ GLCapsule/VertexArray.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: А.Скрипкин
+ * Date: 04.10.2016
+ * Time: 14:01
+ */
+using System;
+using OpenTK.Graphics.OpenGL;
+
+namespace GLCapsule
+{
+    /// <summary>
+    /// Description of VertexArray.
+    /// </summary>
+    public class VertexArray : GLObject
+    {
+        private UInt32 handle;
+        
+        public VertexArray()
+        {
+            Release = () => GL.DeleteVertexArrays(1, ref handle);
+            GL.GenVertexArrays(1,out handle);
+        }
+        
+        public void Bind()
+        {
+            GL.BindVertexArray(handle);
+        }
+    }
+}
